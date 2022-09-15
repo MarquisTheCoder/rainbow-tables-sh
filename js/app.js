f8ab9a0db7e2366663b2a838fec3a47b683d15b3
Deshawn Williams <deshawn.williams516@icloud.com>
2022-09-15 10:54:56 -0500

    initial commit just the styling of the web app

---
+++ js/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+ Creator: Deshawn Marquis, Williams,
+ GitHub Profile: https://github.com/MarquisTheCoder,
+ Date Created: 9/15/22,
+ Time Created: 10:40 AM,
+ File Name: app.js,
+ File Description:
+ *
+*/
+
+let fileButton = document.getElementById('generate-hash-button');
+let fileInput = document.getElementById('get-file');
+fileButton.onclick = function(){
+     fileInput.click();
+}
+
